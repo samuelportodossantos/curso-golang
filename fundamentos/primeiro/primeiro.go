ad2e21dd2676bb559e9b165f0458df86c9c058fb
Samuel P. Santos <samuelportodossantos@gmail.com>
2020-09-18 00:18:43 -0300

    Iniciando curso de golang

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+/*
+Os códigos Go são organizados em pacotes
+e para usá-los é necessário declarar um ou varios imports
+*/
+
+//A funcção main é a porta de entrada de um programa Go (Assim como no java)
+func main() {
+
+	//fmt.Print("Eai galerinha!\n")
+	fmt.Print("Eaii galerinha dois!")
+
+	/*
+		1) priorize código legível e faça comentários que agrega valor!
+		2) Evite comentários obvios
+		3) Durante o curso abuse dos comentários
+	*/
+
+}
