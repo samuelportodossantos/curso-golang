96ecad3597a9a5491d3f2521b787148124a89831
Samuel P. Santos <samuelportodossantos@gmail.com>
2020-09-22 23:14:00 -0300

    Nao tem operador ternario

---
+++ fundamentos/naoternario/naoternario.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// não tem operador ternario em golang
+func obterResultado(nota float64) string {
+	if nota >= 6 {
+		return "Aprovado"
+	}
+	return "Reprovado"
+}
+
+func main() {
+	fmt.Println(obterResultado(6.2))
+}
